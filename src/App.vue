75fe31c5d6d558dfe6df34d8f6f8a52bfa6b7e41
Rikitariko <leoprogbars@gmail.com>
2020-09-30 00:39:20 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="main" v-bind:class="{ active: toolbarStatus }">
+      <Header />
+    </div>
+    <SideBar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header/Header.vue";
+import SideBar from "@/components/SideBar/SideBar.vue";
+
+export default {
+  beforeCreate() {
+    this.$store.dispatch("setInitialValue");
+  },
+  computed: {
+    toolbarStatus: function () {
+      return this.$store.getters.getActiveToolBar;
+    },
+  },
+  components: {
+    Header,
+    SideBar
+  }
+};
+</script>
+
+<style>
+@import "./app.scss";
+</style>
